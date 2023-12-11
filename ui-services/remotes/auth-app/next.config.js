5afa8c03420fe23697b9169eafb3976904ab6dbc
karpagavinayagam thangavelu <karpagavinayagam.thangavelu@gmail.com>
2023-12-11 19:46:33 +0530

    [SW-1] - Auth base setup

--- ui-services/remotes/auth-app/next.config.js
+++ ui-services/remotes/auth-app/next.config.js
@@ -1,7 +1,19 @@
-//@ts-check
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+const remotes = (isServer) => {
+  const location = isServer ? 'ssr' : 'chunks';
+
+  return {
+    store: `store@${process.env.NEXT_PUBLIC_STORE_URL}/_next/static/${location}/remoteEntry.js`,
+  };
+};
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -11,6 +23,30 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  name: 'auth',
+   /**
+   *
+   * @param {import('webpack').Configuration} config
+   * @returns {import('webpack').Configuration}
+   */
+   webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'checkout',
+        filename: 'static/chunks/remoteEntry.js',
+        remotes: {},
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+        exposes: {
+          './buy-button': './components/buy-button/buy-button.tsx',
+        },
+        shared: {},
+      })
+    );
+
+    return config;
   },
 };
 
